8cc29e685e50c8a1db4ac940b4762e10ace79650
Stefan Andrei <stefandrei123@gmail.com>
2020-10-06 17:15:39 +0200

    created navbar and me query

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <div>Hello World</div>;
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+  <div>
+    <NavBar />
+    Hello World
+  </div>
+);
 
 export default Index;
