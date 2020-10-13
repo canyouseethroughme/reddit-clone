afbf747cba8856403c8d82f41c9b66b1d1a0b798
Stefan Andrei <stefandrei123@gmail.com>
2020-10-13 17:40:04 +0200

    refactored and created query for posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <div>
-    <NavBar />
-    Hello World
-  </div>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <div>
+        <NavBar />
+        Hello World
+      </div>
+      <br />
+      {!data ? null : data?.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -6,6 +6,8 @@ import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface registerProps {}
 
@@ -18,7 +20,6 @@ const Register: React.FC<registerProps> = ({}) => {
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
-          console.log("salam", response);
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
@@ -56,4 +57,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default withUrqlClient(createUrqlClient)(Register);
