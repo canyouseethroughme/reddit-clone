92a14a326b47c61dadfe3c5c6a7ea40bba1ee47f
Stefan Andrei <stefandrei123@gmail.com>
2020-10-13 16:40:09 +0200

    created logout mutation

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Button, Flex, Link } from "@chakra-ui/core";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
 
   let body = null;
@@ -29,7 +30,15 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={2}>{data.me.username}</Box>
-        <Button variant="link">logout</Button>
+        <Button
+          variant="link"
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+        >
+          logout
+        </Button>
       </Flex>
     );
   }
