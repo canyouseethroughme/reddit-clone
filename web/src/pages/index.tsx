21795781ab511a67ac4a0cb57ee7986a85351015
Stefan Andrei <stefandrei123@gmail.com>
2020-10-14 00:00:17 +0200

    nodemailer setup and register with email

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,14 +6,11 @@ import { usePostsQuery } from "../generated/graphql";
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <div>
-        <NavBar />
-        Hello World
-      </div>
-      <br />
+    <div>
+      <NavBar />
+      <div>Hello World</div>
       {!data ? null : data?.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </>
+    </div>
   );
 };
 
